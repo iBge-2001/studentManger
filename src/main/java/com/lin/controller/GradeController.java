69b141e80a6455a390bef387df4556fe29a0cacc
weixin_58376326 <weixin_58376326@gitcode.net>
2023-04-17 22:46:48 +0800

    无

--- src/main/java/com/lin/controller/GradeController.java
+++ src/main/java/com/lin/controller/GradeController.java
@@ -21,4 +21,5 @@ public class GradeController {
         return new Result(code,course,msg);
     }
 
+
 }
--- src/main/java/com/lin/dao/GradeDao.java
+++ src/main/java/com/lin/dao/GradeDao.java
@@ -8,5 +8,4 @@ public interface GradeDao {
     @Select("select course from coursebyprofession where st_profession =#{st_profession}")
     public List<String> GetCourse(String st_profession);
 
-
 }
