142df003cb91701e27578aa3e76cc9cc0f976507
chenzikai <1449316752@qq.com>
2023-04-19 11:16:33 +0800

    首页登录跳转、根路径跳转

--- src/main/java/com/lin/controller/LoginController.java
+++ src/main/java/com/lin/controller/LoginController.java
@@ -4,12 +4,10 @@ import com.lin.domain.User;
 import com.lin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/login")
@@ -17,8 +15,8 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
-    @PostMapping()
-    public Result userLogin(HttpServletRequest request, @RequestBody User user){
+    @PostMapping
+    public Result userLogin(HttpSession session, @RequestBody User user){
         User user1 = userService.selectOneUser(user.getUser());
         System.out.println("*--------------------*");
         if (user1 == null){
@@ -27,8 +25,28 @@ public class LoginController {
         if (!user1.getPassword().equals(user.getPassword())){
             return new Result(Code.POST_ERR,null,"账号或密码出错");
         }
-        request.getSession().setAttribute("user",user.getUser());
+        session.setAttribute("user",user);
         return new Result(Code.POST_OK,null,"登录成功");
     }
+
+    @GetMapping
+    public Result home(@SessionAttribute(required = false) User user) {
+        System.out.println(user);
+        if (user != null) {
+            return new Result(Code.GET_OK,user,"已登录");
+        } else {
+            return new Result(Code.GET_ERR,null,"未登录");
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        System.out.println("logout");
+        // 清除Session中的用户信息
+        session.removeAttribute("user");
+        // 返回登录页面
+        return "redirect:/login";
+    }
+
 
 }
